1d4185d64c2ecc6a620892c2ef9849e54e6bd2f6
JiSuMun <mjs3827@naver.com>
2023-05-30 22:49:13 +0900

    add host

--- planethelper/settings.py
+++ planethelper/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost',]
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'port-0-test-deploy-das6e2dliabwwlt.sel4.cloudtype.app',]
 
 
 # Application definition
